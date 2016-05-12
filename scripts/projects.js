e92d31c418ce33e83452b0a97618144d4218748a
keiran beaton <keiran beaton>
2016-05-11 17:37:41 -0700

    fixing assignment from yesterday to meet technical requirements

---
+++ scripts/projects.js
@@ -0,0 +1,20 @@
+var projectsArray = [
+  {
+    title:  'project1',
+    category: 'html',
+    createdOn:  '2016-05-10',
+    body:  '<p>Body text goes here</p>'
+  },
+  {
+    title:  'project2',
+    category: 'css',
+    createdOn:  '2016-05-10',
+    body:  '<p>Body text goes here</p>'
+  },
+  {
+    title:  'project3',
+    category: 'js',
+    createdOn:  '2016-05-10',
+    body:  '<p>Body text goes here</p>'
+  }
+]
