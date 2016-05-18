6794e238f44407ddc827726aa094ee9673245788
keiran beaton <keiran beaton>
2016-05-17 20:33:47 -0700

    added IIFEs, not sure where to put map and reduce functions

--- scripts/project.js
+++ scripts/project.js
@@ -1,32 +1,37 @@
-function Project (opts) {
-  for(var prop in opts) {this[prop] = opts[prop];}
-}
+(function (module) {
 
-Project.all = [];
-
-Project.prototype.toHtml = function(templateId) {
-  var template = Handlebars.compile((templateId).html());
-  return template(this);
-};
-
-Project.loadAll = function(data) {
-  data.sort(function(a,b) {
-    return (new Date(b.createdOn)) - (new Date(a.createdOn));
-  });
-  data.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function() {
-  if (localStorage.projectsData) {
-    Project.loadAll(JSON.parse(localStorage.projectsData));
-    projectView.initIndexPage();
-  } else {
-    $.getJSON('data/projectsData.json',function(data){
-      Project.loadAll(data);
-      localStorage.projectsData = JSON.stringify(data);
-      projectView.initIndexPage();
-    });
+  function Project (opts) {
+    for(var prop in opts) {this[prop] = opts[prop];}
   }
-};
+
+  Project.all = [];
+
+  Project.prototype.toHtml = function(templateId) {
+    var template = Handlebars.compile((templateId).html());
+    return template(this);
+  };
+
+  Project.loadAll = function(data) {
+    data.sort(function(a,b) {
+      return (new Date(b.createdOn)) - (new Date(a.createdOn));
+    });
+    data.forEach(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  };
+
+  Project.fetchAll = function() {
+    if (localStorage.projectsData) {
+      Project.loadAll(JSON.parse(localStorage.projectsData));
+      projectView.initIndexPage();
+    } else {
+      $.getJSON('data/projectsData.json',function(data){
+        Project.loadAll(data);
+        localStorage.projectsData = JSON.stringify(data);
+        projectView.initIndexPage();
+      });
+    }
+  };
+
+  module.Project = Project;
+})(window);
--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,45 +1,48 @@
-var projectView = {};
+(function(module) {
+  var projectView = {};
 
-projectView.handleNavBar = function() {
-  $('.nav-bar').on('click', '.nav-tab', function(){
-    $('.content-section').hide();
-    var chosenTab = $(this).attr('data-content');
-    $('section[id = "' + chosenTab + '"]').fadeIn();
-  });
-  $('.nav-bar .nav-tab:first').click();
-};
+  projectView.handleNavBar = function() {
+    $('.nav-bar').on('click', '.nav-tab', function(){
+      $('.content-section').hide();
+      var chosenTab = $(this).attr('data-content');
+      $('section[id = "' + chosenTab + '"]').fadeIn();
+    });
+    $('.nav-bar .nav-tab:first').click();
+  };
 
-projectView.handleFilter = function() {
-  $('#category-filter').on('change', function() {
-    if($(this).val()) {
-      $('.project').hide();
-      $('.project[data-category="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('.project').fadeIn();
-      $('article.template').hide();
-    }
-  });
-};
+  projectView.handleFilter = function() {
+    $('#category-filter').on('change', function() {
+      if($(this).val()) {
+        $('.project').hide();
+        $('.project[data-category="' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('.project').fadeIn();
+        $('article.template').hide();
+      }
+    });
+  };
 
-projectView.setTeasers = function() {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  $('article').on('click', 'a', function(e){
-    e.preventDefault();
-    var parentArticle = $(this).parent();
-    var childSection = parentArticle.children('.article-body');
-    $(childSection).children().show();
-    $(this).hide();
-  });
-};
+  projectView.setTeasers = function() {
+    $('.article-body *:nth-of-type(n+2)').hide();
+    $('article').on('click', 'a', function(e){
+      e.preventDefault();
+      var parentArticle = $(this).parent();
+      var childSection = parentArticle.children('.article-body');
+      $(childSection).children().show();
+      $(this).hide();
+    });
+  };
 
-projectView.initIndexPage = function() {
-  Project.all.forEach(function(a){
-    if($('#category-filter option:contains("'+ a.category + '")').length === 0) {
-      $('#category-filter').append(a.toHtml($('#filter-template')));
-    };
-    $('#projects-section').append(a.toHtml($('#template')));
-  });
-  projectView.handleNavBar();
-  projectView.handleFilter();
-  projectView.setTeasers();
-};
+  projectView.initIndexPage = function() {
+    Project.all.forEach(function(a){
+      if($('#category-filter option:contains("'+ a.category + '")').length === 0) {
+        $('#category-filter').append(a.toHtml($('#filter-template')));
+      };
+      $('#projects-section').append(a.toHtml($('#template')));
+    });
+    projectView.handleNavBar();
+    projectView.handleFilter();
+    projectView.setTeasers();
+  };
+  module.projectView = projectView;
+})(window);
